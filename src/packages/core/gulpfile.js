0c83033e461e3bd26daa7805377c24b3a916c2a6
Dmytro <dimagolyak@gmail.com>
2023-01-09 15:03:15 +0200

    Added base file and structure

--- src/packages/core/gulpfile.js
+++ src/packages/core/gulpfile.js
@@ -1,14 +1,16 @@
 const { src, dest, watch, series } = require('gulp');
+const purgecss = require('gulp-purgecss');
 const sass = require('gulp-sass')(require('sass'));
 
 function buildStyles() {
-    return src('index.scss')
+    return src('**/*.scss')
         .pipe(sass())
+        .pipe(purgecss({content: ['*.html']}))
         .pipe(dest('css'))
 }
 
 function watchTask() {
-    watch(['index.scss'], buildStyles)
+    watch(['**/*.scss'], buildStyles)
 }
 
 exports.default = series(buildStyles, watchTask);
