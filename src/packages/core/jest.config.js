e1e7f3dd3def4cc7d0fd76bb31a1e72b627001bc
Dmytro <dimagolyak@gmail.com>
2023-02-03 23:26:43 +0200

    Added crads

--- src/packages/core/jest.config.js
+++ src/packages/core/jest.config.js
@@ -2,6 +2,5 @@ module.exports = {
   displayName: '@construct-flow/core',
   testMatch: ['<rootDir>/__tests__/**/*.test.js'],
   testPathIgnorePatterns: ['packages/core-react', 'packages/core-angular'],
-  // testEnvironment: 'node',
   testEnvironment: 'jest-environment-node-single-context',
 };
